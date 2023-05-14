9c51a22de5ae8040b97c0f6ded53a233733498ca
STFU <77781079+V01D-co@users.noreply.github.com>
2023-05-15 06:53:56 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Ryzen Kernel by Edwiin Kusuma Jaya @ telegram
+kernel.string=SuperV01D
 do.devicecheck=1
 do.modules=1
 do.systemless=0
